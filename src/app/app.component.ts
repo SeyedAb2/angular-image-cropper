0411730ca1c8a5ae90758ec4653258f5f9ea0a65
abbasam <tiggerwitht1378@gmail.com>
2025-04-27 13:27:33 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { PinturaEditorComponent } from '@pqina/angular-pintura';
+import { getCropperDefaults, getEditorDefaults, PinturaEditorOptions } from '@pqina/pintura';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  @ViewChild('editorRef') editorRef?: PinturaEditorComponent<any> = undefined;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  editorOptions = getEditorDefaults() as PinturaEditorOptions;
+
+  src: string = 'assets/1.jpg';
+  result?: string = undefined;
+  cropAspectRatio = 1;
+  locale?: any = { ...getEditorDefaults().locale };
+
+  handleLoad($event: any) {
+    console.log('load', $event);
+
+    console.log('component ref', this.editorRef);
+
+    console.log('editor instance ref', this.editorRef?.editor);
+
+    console.log(
+      'inline editor image state',
+      this.editorRef?.editor?.imageState
+    );
+  }
+
+  handleProcess($event: any) {
+    console.log('process', $event);
+
+    const objectURL = URL.createObjectURL($event.dest);
+    this.result = this.sanitizer.bypassSecurityTrustResourceUrl(
+      objectURL
+    ) as string;
+  }
+
+  handleChangeLocale($event: any) {
+    // load german locale
+    import('@pqina/pintura/locale/nl_NL/index.js').then(
+      ({ default: locale }:any) => {
+        this.locale = locale;
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AngularPinturaModule } from '@pqina/angular-pintura';
+import { FilePondModule } from 'ngx-filepond';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularPinturaModule,
+    FilePondModule
   ],
   providers: [],
   bootstrap: [AppComponent]
